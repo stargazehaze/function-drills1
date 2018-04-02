0889e774cd3ec5ff0dfc65c1adb29775355e88df
Jennifer Flynn <jflynn581@gmail.com>
2018-04-02 16:31:56 -0400

    completed JS drills 1

--- first.js
+++ first.js
@@ -1,6 +1,35 @@
 'use strict';
 
 function whoAmI(name, age) {
-    console.log(`Hi, my name is ${name} and I'm ${age} years old.);
+  if (name === undefined) {
+    console.error('Arguments not valid');
+  }  else if (age === undefined) {
+    console.error('Arguments not valid');
+  }
+
+  if (typeof name !== String) {
+      console.error('Name must be a string.');
+  } else if (typeof age !== Number) {
+      console.error('Age must be a number.');
+  }
+
+  let yearOfBirth = 2018 - age;
+  console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
+  console.log(`I was born in ${yearOfBirth}`);
 }
 
+whoAmI(34, false);
+
+function yearOfBirth(age) {
+  try {
+    yearOfBirth < 2018;
+    throw new Error('Age can not be negative.');
+  } catch (e) {
+    console.log(e); 
+  }
+
+  return 2018 - age; 
+ 
+}
+
+yearOfBirth(-25);
